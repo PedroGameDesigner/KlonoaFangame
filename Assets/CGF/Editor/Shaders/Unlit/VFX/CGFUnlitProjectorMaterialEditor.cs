88f36af0785c5623e5e9dec37c8c79c8c62fda47
PedroGameDesigner <pedro.game.designer@gmail.com>
2022-06-18 16:31:16 +0200

    Klonoa Shadow more or less working

---
+++ Assets/CGF/Editor/Shaders/Unlit/VFX/CGFUnlitProjectorMaterialEditor.cs
@@ -0,0 +1,122 @@
+﻿///
+/// INFORMATION
+/// 
+/// Project: Chloroplast Games Framework
+/// Game: Chloroplast Games Framework
+/// Date: 05/09/2018
+/// Author: Chloroplast Games
+/// Website: http://www.chloroplastgames.com
+/// Programmers: Pau Elias Soriano
+/// Description: Editor of material with shader Unlit/VFX/Projector.
+///
+
+using UnityEngine;
+using UnityEditor;
+using CGF.Systems.Shaders.Unlit.VFX;
+
+/// \english
+/// <summary>
+/// Editor of material with shader Unlit/VFX/Projector.
+/// </summary>
+/// \endenglish
+/// \spanish
+/// <summary>
+/// Editor del material con el shader Unlit/VFX/Projector.
+/// </summary>
+/// \endspanish
+public class CGFUnlitProjectorMaterialEditor : CGFMaterialEditorClass
+{
+
+    #region Private Variables
+
+    private bool _compactMode;
+
+    private bool _blendTypeHeaderGroup = true;
+
+    private bool _projectorHeaderGroup = true;
+
+    private bool _uvScrollHeaderGroup;
+
+    // Projector
+    MaterialProperty _ShadowTex;
+    MaterialProperty _ShadowTexTiling;
+    MaterialProperty _ShadowTexOffset;
+    MaterialProperty _Tint;
+    MaterialProperty _ShadowLevel;
+    MaterialProperty _FalloffMap;
+    MaterialProperty _BackfaceCulling;
+    MaterialProperty _UseVertexPosition;
+
+    // UV Scroll
+    MaterialProperty _UVScroll;
+    MaterialProperty _FlipUVHorizontal;
+    MaterialProperty _FlipUVVertical;
+    MaterialProperty _UVScrollAnimation;
+    MaterialProperty _UVScrollSpeed;
+
+    // Blend Type
+    MaterialProperty _BlendType;
+    MaterialProperty _BlendFactorSource;
+    MaterialProperty _BlendFactorDestination;
+    MaterialProperty _BlendOperation;
+
+    #endregion
+
+
+    #region Main Methods
+
+    protected override void GetProperties()
+    {
+
+        // Projector
+        _ShadowTex = FindProperty("_ShadowTex");
+        _ShadowTexTiling = FindProperty("_ShadowTexTiling");
+        _ShadowTexOffset = FindProperty("_ShadowTexOffset");
+        _Tint = FindProperty("_Tint");
+        _ShadowLevel = FindProperty("_ShadowLevel");
+        _FalloffMap = FindProperty("_FalloffMap");
+        _BackfaceCulling = FindProperty("_BackfaceCulling");
+        _UseVertexPosition = FindProperty("_UseVertexPosition");
+
+        // UV Scroll
+        _UVScroll = FindProperty("_UVScroll");
+        _FlipUVHorizontal = FindProperty("_FlipUVHorizontal");
+        _FlipUVVertical = FindProperty("_FlipUVVertical");
+        _UVScrollAnimation = FindProperty("_UVScrollAnimation");
+        _UVScrollSpeed = FindProperty("_UVScrollSpeed");
+
+        // Blend Type
+        _BlendType = FindProperty("_BlendType");
+        _BlendFactorSource = FindProperty("_SrcBlendFactor");
+        _BlendFactorDestination = FindProperty("_DstBlendFactor");
+        _BlendOperation = FindProperty("_BlendOperation");
+
+    }
+
+    protected override void InspectorGUI()
+    {
+        CGFMaterialEditorUtilitiesClass.BuildMaterialComponent(typeof(CGFUnlitProjectorBehavior));
+
+        CGFMaterialEditorUtilitiesClass.BuildMaterialTools("http://chloroplastgames.com/cg-framework-user-manual/");
+
+        CGFMaterialEditorUtilitiesClass.ManageMaterialValues(this);
+
+        _compactMode = CGFMaterialEditorUtilitiesClass.BuildTextureCompactMode(_compactMode, m_HeaderStateKey);
+
+        GUILayout.Space(5);
+
+        CGFMaterialEditorUtilitiesClass.BuildBlendTypeEnum(_BlendType, _BlendFactorSource, _BlendFactorDestination, _BlendOperation, this, _blendTypeHeaderGroup, m_HeaderStateKey);
+
+        GUILayout.Space(5);
+
+        // Projector
+        CGFMaterialEditorUtilitiesExtendedClass.BuildProjector(_ShadowTex, _ShadowTexTiling, _ShadowTexOffset, _Tint, _ShadowLevel, _FalloffMap, _BackfaceCulling, _UseVertexPosition, this, _projectorHeaderGroup, m_HeaderStateKey, _compactMode);
+
+        GUILayout.Space(5);
+
+        // UV Scroll
+        CGFMaterialEditorUtilitiesExtendedClass.BuildUVScrollSimple(_UVScroll, _FlipUVHorizontal, _FlipUVVertical, _UVScrollAnimation, _UVScrollSpeed, _uvScrollHeaderGroup, m_HeaderStateKey);
+
+    }
+    #endregion
+}
---
+++ Assets/CGF/Systems/Shaders/Unlit/VFX/CGFUnlitProjectorBehavior.cs
@@ -0,0 +1,388 @@
+﻿///
+/// INFORMATION
+/// 
+/// Project: Chloroplast Games Framework
+/// Game: Chloroplast Games Framework
+/// Date: 02/03/2019
+/// Author: Chloroplast Games
+/// Web: http://www.chloroplastgames.com
+/// Programmers: David Cuenca
+/// Description: Behavior that allows to the attatched gameobject manage a material with shader Unlit/Projector.
+///
+
+using UnityEngine;
+using CGF.Systems.Shaders;
+
+namespace CGF.Systems.Shaders.Unlit.VFX
+{
+
+    /// \english
+    /// <summary>
+    /// Behavior that allows to the attatched gameobject manage a material with shader Unlit/Projector.
+    /// </summary>
+    /// \endenglish
+    /// \spanish
+    /// <summary>
+    /// Comportamiento que permite al gameobject asociado gestionar un material con el shader Unlit/Projector.
+    /// </summary>
+    /// \endspanish
+    [ExecuteInEditMode]
+    public class CGFUnlitProjectorBehavior : CGFShaderBehavior
+    {
+
+        #region Public Variables
+
+        public float BlendType
+        {
+            get { return _blendType; }
+            set
+            {
+                _blendType = value;
+                SetFloat("_BlendType", _blendType);
+            }
+        }
+
+        public float BlendSource
+        {
+            get { return _blendSource; }
+            set
+            {
+                _blendSource = value;
+                SetFloat("_SrcBlendFactor", _blendSource);
+            }
+        }
+
+        public float BlendDestination
+        {
+            get { return _blendDestination; }
+            set
+            {
+                _blendDestination = value;
+                SetFloat("_DstBlendFactor", _blendDestination);
+            }
+        }
+
+        public float Cutoff
+        {
+            get { return _cutoff; }
+            set
+            {
+                _cutoff = value;
+                SetFloat("_Cutoff", _cutoff);
+            }
+        }
+
+        public Color CookieColor
+        {
+            get { return _cookieColor; }
+            set
+            {
+                _cookieColor = value;
+                SetColor("_Tint", _cookieColor);
+            }
+        }
+
+        public Texture ShadowTex
+        {
+            get { return _shadowTex; }
+            set
+            {
+                _shadowTex = value;
+                SetTexture("_ShadowTex", _shadowTex);
+            }
+        }
+
+        public Vector2 ShadowTexTiling
+        {
+            get { return _shadowTexTiling; }
+            set
+            {
+                _shadowTexTiling = value;
+                SetVector("_ShadowTexTiling", _shadowTexTiling);
+            }
+        }
+
+        public Vector2 ShadowTexOffset
+        {
+            get { return _shadowTexOffset; }
+            set
+            {
+                _shadowTexOffset = value;
+                SetVector("_ShadowTexOffset", _shadowTexOffset);
+            }
+        }
+
+        public bool UvScrollAnimation
+        {
+            get { return _uvScrollAnimation; }
+            set
+            {
+                _uvScrollAnimation = value;
+                SetFloat("_UVScrollAnimation", _uvScrollAnimation);
+            }
+        }
+
+        public float ShadowLevel
+        {
+            get { return _shadowLevel; }
+            set
+            {
+                _shadowLevel = value;
+                SetFloat("_ShadowLevel", _shadowLevel);
+            }
+        }
+
+        public Vector2 UvScrollSpeed
+        {
+            get { return _uvScrollSpeed; }
+            set
+            {
+                _uvScrollSpeed = value;
+                SetVector("_UVScrollSpeed", _uvScrollSpeed);
+            }
+        }
+
+        public bool ScrollByTexel
+        {
+            get { return _scrollByTexel; }
+            set
+            {
+                _scrollByTexel = value;
+                SetFloat("_ScrollByTexel", _scrollByTexel);
+            }
+        }
+
+        public Texture FalloffMap
+        {
+            get { return _falloffMap; }
+            set
+            {
+                _falloffMap = value;
+                SetTexture("_FalloffMap", _falloffMap);
+            }
+        }
+
+        public bool BackfaceCulling
+        {
+            get { return _backfaceCulling; }
+            set
+            {
+                _backfaceCulling = value;
+                SetFloat("_BackfaceCulling", _backfaceCulling);
+            }
+        }
+
+        public bool UseVertexPosition
+        {
+            get { return _useVertexPosition; }
+            set
+            {
+                _useVertexPosition = value;
+                SetFloat("_UseVertexPosition", _useVertexPosition);
+            }
+        }
+
+        public bool FlipUVVertical
+        {
+            get { return _flipUVVertical; }
+            set
+            {
+                _flipUVVertical = value;
+                SetFloat("_FlipUVVertical", _flipUVVertical);
+            }
+        }
+
+        public bool FlipUVHorizontal
+        {
+            get { return _flipUVHorizontal; }
+            set
+            {
+                _flipUVHorizontal = value;
+                SetFloat("_FlipUVHorizontal", _flipUVHorizontal);
+            }
+        }
+
+        public bool UvScroll
+        {
+            get { return _uvScroll; }
+            set
+            {
+                _uvScroll = value;
+                SetFloat("_UVScroll", _uvScroll);
+            }
+        }
+
+        #endregion
+
+
+        #region Private Variables
+
+        [SerializeField]
+        private float _blendType;
+
+        [SerializeField]
+        private float _blendSource;
+
+        [SerializeField]
+        private float _blendDestination;
+
+        [SerializeField]
+        private float _cutoff;
+
+        [SerializeField]
+        private Color _cookieColor;
+
+        [SerializeField]
+        private Texture _shadowTex;
+
+        [SerializeField]
+        private Vector2 _shadowTexTiling;
+
+        [SerializeField]
+        private Vector2 _shadowTexOffset;
+
+        [SerializeField]
+        private bool _uvScrollAnimation;
+
+        [SerializeField]
+        private float _shadowLevel;
+
+        [SerializeField]
+        private Vector2 _uvScrollSpeed;
+
+        [SerializeField]
+        private bool _scrollByTexel;
+
+        [SerializeField]
+        private Texture _falloffMap;
+
+        [SerializeField]
+        private bool _backfaceCulling;
+
+        [SerializeField]
+        private bool _useVertexPosition;
+
+        [SerializeField]
+        private bool _flipUVVertical;
+
+        [SerializeField]
+        private bool _flipUVHorizontal;
+
+        [SerializeField]
+        private bool _uvScroll;
+
+        #endregion
+
+
+        #region Main Methods
+
+        protected override void Awake()
+        {
+
+            base.Awake();
+
+            _shaderName = "CG Framework/Unlit/Unlit Projector";
+
+        }
+
+        #endregion
+
+
+        #region Utility Methods
+
+        protected override void CopyShaderParameter()
+        {
+
+            base.CopyShaderParameter();
+
+            Cutoff = GetFloat("_Cutoff"); ;
+
+            CookieColor = GetColor("_Tint"); ;
+
+            ShadowTex = GetTexture("_ShadowTex"); ;
+
+            ShadowTexOffset = GetVector("_ShadowTexOffset"); ;
+
+            ShadowTexTiling = GetVector("_ShadowTexTiling"); ;
+
+            UvScrollAnimation = GetBoolean("_UVScrollAnimation"); ;
+
+            ShadowLevel = GetFloat("_ShadowLevel"); ;
+
+            UvScrollSpeed = GetVector("_UVScrollSpeed"); ;
+
+            ScrollByTexel = GetBoolean("_ScrollByTexel"); ;
+
+            FalloffMap = GetTexture("_FalloffMap"); ;
+
+            BackfaceCulling = GetBoolean("_BackfaceCulling"); ;
+
+            UseVertexPosition = GetBoolean("_UseVertexPosition"); ;
+
+            BlendType = GetFloat("_BlendType"); ;
+
+            BlendSource = GetFloat("_SrcBlendFactor"); ;
+
+            BlendDestination = GetFloat("_DstBlendFactor"); ;
+
+            FlipUVVertical = GetBoolean("_FlipUVVertical"); ;
+
+            FlipUVHorizontal = GetBoolean("_FlipUVHorizontal"); ;
+
+            UvScroll = GetBoolean("_UVScroll"); ;
+
+        }
+
+        protected override void SetShaderParameters()
+        {
+
+            base.SetShaderParameters();
+
+            Cutoff = _cutoff;
+
+            CookieColor = _cookieColor;
+
+            ShadowTex = _shadowTex;
+
+            ShadowTexOffset = _shadowTexOffset;
+
+            ShadowTexTiling = _shadowTexTiling;
+
+            UvScrollAnimation = _uvScrollAnimation;
+
+            ShadowLevel = _shadowLevel;
+
+            UvScrollSpeed = _uvScrollSpeed;
+
+            ScrollByTexel = _scrollByTexel;
+
+            FalloffMap = _falloffMap;
+
+            BackfaceCulling = _backfaceCulling;
+
+            UseVertexPosition = _useVertexPosition;
+
+            BlendType = _blendType;
+
+            BlendSource = _blendSource;
+
+            BlendDestination = _blendDestination;
+
+            FlipUVVertical = _flipUVVertical;
+
+            FlipUVHorizontal = _flipUVHorizontal;
+
+            UvScroll = _uvScroll;
+
+        }
+
+        #endregion
+
+
+        #region Utility Events
+
+        #endregion
+
+    }
+
+}
