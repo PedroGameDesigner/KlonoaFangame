69129ec8f991bbed9fd9576a65f3b7b4df011845
PedroGameDesigner <pedro.game.designer@gmail.com>
2022-09-03 16:29:51 +0200

    Dreamstone collection effect

--- Assets/_Core/Scripts/Gameplay/Collectables/DreamStone.cs
+++ Assets/_Core/Scripts/Gameplay/Collectables/DreamStone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Gameplay.Collectables
 {
@@ -10,12 +11,28 @@ namespace Gameplay.Collectables
         public static event StoneDelegate StoneCollectedEvent;
 
         [SerializeField] private int _value = 1;
+        [Space]
+        [SerializeField] SpriteRenderer _renderer = null;
+        [SerializeField] ParticleSystem _collectParticles = null;
+        [Space]
+        [SerializeField] private float _animationDuration = 1;
+        [SerializeField] private float _rotationSpeed = 1;
 
         public int Value => _value;
+        public float CollectionDuration => _collectParticles.main.duration;
 
         protected override void InvokeCollectionEvent()
         {
             StoneCollectedEvent?.Invoke(this);
+        }
+
+        protected override void StartDestruction()
+        {
+            _collider.enabled = false;
+            _renderer.enabled = false;
+            _collectParticles.Play();
+            Destroy(gameObject, CollectionDuration);
+            
         }
     }
 }
