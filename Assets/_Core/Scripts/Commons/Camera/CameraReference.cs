30d34a2bc1e6ff1e666d9d214a1901262f7947b5
Pedro <pedro.game.designer@gmail.com>
2023-12-30 13:25:30 +0100

    Cameras for Section 1 of level 1-1

--- Assets/_Core/Scripts/Commons/Camera/CameraReference.cs
+++ Assets/_Core/Scripts/Commons/Camera/CameraReference.cs
@@ -1,7 +1,4 @@
 using Cinemachine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Cameras
@@ -17,43 +14,17 @@ namespace Cameras
         private static CameraReference PreviousCamera = null;
 
         [SerializeField] private bool _defaultCamera;
-        [SerializeField] private float _transitionTime;
-
-        [Header("Horizontal Settings")]
-        [SerializeField] private float _horizontalDistanceProportion;
-
-        [Header("Vertial Settings")]
-        [SerializeField] private bool _followOnAir = true;
-        [SerializeField] private bool _followOnGround = true;
-        [SerializeField] private bool _followOnEvent = true;
-        [SerializeField] private Vector2 _deathZoneMargin;
 
         private CinemachineVirtualCamera _camera;
-        private CameraTarget _target;
-        private CinemachineFramingTransposer _cameraBody;
-
-        private float DistanceToTarget => _cameraBody.m_CameraDistance;
 
         private void Awake()
         {
             _camera = GetComponent<CinemachineVirtualCamera>();
-            _cameraBody = _camera.GetCinemachineComponent<CinemachineFramingTransposer>();
         }
 
         private void OnValidate()
         {
             _camera = GetComponent<CinemachineVirtualCamera>();
-            _target = FindAnyObjectByType<CameraTarget>();
-            _cameraBody = _camera.GetCinemachineComponent<CinemachineFramingTransposer>();
-            _camera.m_Transitions.m_OnCameraLive.AddListener(OnCameraLive);
-        }
-
-        private void OnCameraLive(ICinemachineCamera cameraIn, ICinemachineCamera cameraOut)
-        {
-            /*
-            _target.FollowOnAir = _followOnAir;
-            _target.FollowOnGround = _followOnGround;
-            _target.UpdateRelativePosition(DistanceToTarget, _transitionTime);*/
         }
 
         public void ToggleCamera()
